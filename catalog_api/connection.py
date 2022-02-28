7e464f2c02478945743a83481c0ebc9a84dfbf6f
alfisnig <valerasafin2005@gmail.com>
2022-02-28 23:30:12 +0300

    execute_sql function added

---
+++ catalog_api/connection.py
@@ -0,0 +1,23 @@
+import logging
+import sqlite3
+from constants import CATALOG_DB_PATH
+
+
+def create_database():
+    connection = sqlite3.connect(CATALOG_DB_PATH)
+    connection.close()
+
+
+def execute_sql(sql_expression: str, parameters: dict = None) -> sqlite3.Cursor:
+    with sqlite3.connect(CATALOG_DB_PATH) as connection:
+        try:
+            if parameters is None:
+                result = connection.execute(sql_expression)
+            else:
+                result = connection.execute(sql_expression, parameters)
+        except Exception as e:
+            logging.error("Ошибка: ", exc_info=True)
+            connection.rollback()
+        else:
+            connection.commit()
+            return result
