c546c6f40daa8ae2abdd0915d0f31555f81f3784
alfisnig <valerasafin2005@gmail.com>
2022-02-28 23:28:08 +0300

    new constants added

--- constants.py
+++ constants.py
@@ -3,3 +3,6 @@ import os
 
 ROOT_DIR = os.path.dirname(os.path.abspath(__file__))
 CATALOG_DB_NAME = "catalog.db"
+CATALOG_DB_PATH = os.path.join(ROOT_DIR, CATALOG_DB_NAME)
+LOG_FILE_NAME = "logs.log"
+LOG_FILE_PATH = os.path.join(ROOT_DIR, LOG_FILE_NAME)
