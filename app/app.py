5d21f28e6a91e7e68444a1cc62f29786766e0ea4
alfisnig <valerasafin2005@gmail.com>
2022-02-28 23:34:53 +0300

    add connections in gui

--- app/app.py
+++ app/app.py
@@ -1,6 +1,8 @@
+import logging
 from PySide6.QtWidgets import QApplication, QWidget, QPushButton, QMainWindow
 from .gui import ControlPanelWindow
 from .bot_controller import BotController
+from catalog_api import CatalogController
 
 
 class App:
@@ -8,19 +10,22 @@ class App:
         self._app = QApplication()
         self.control_panel: ControlPanelWindow = None
         self._bot_controller: BotController = None
+        self._catalog_controller: CatalogController = None
 
     def init(self):
         self.control_panel = ControlPanelWindow()
         self.control_panel.show()
         self._bot_controller = BotController()
+        self._catalog_controller = CatalogController()
         self.init_connections()
 
     def init_connections(self):
         self.control_panel.start_bot.connect(self._bot_controller.start_bot_process)
         self.control_panel.stop_bot.connect(self._bot_controller.stop_bot_process)
+        self.control_panel.add_catalog.connect(self._catalog_controller.load_catalog)
 
     def exec(self):
         try:
             return self._app.exec()
         except Exception:
-            return self._app.exec_()
+            logging.error("Ошибка: ", exc_info=True)
