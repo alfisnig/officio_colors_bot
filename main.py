17eab76a4dc443f2b9d32caed66fe400bc248668
alfisnig <valerasafin2005@gmail.com>
2022-03-01 00:43:13 +0300

    debug level changed

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ from catalog_api import init_database
 
 
 logging.getLogger(LOG_FILE_PATH)
-logging.basicConfig(filename=LOG_FILE_PATH, level=logging.DEBUG, format='%(asctime)s - %(message)s',
+logging.basicConfig(filename=LOG_FILE_PATH, level=logging.ERROR, format='%(asctime)s - %(message)s',
                     datefmt='%d-%b-%y %H:%M:%S', filemode="a")
 init_database()
 
