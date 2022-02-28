e3389192307e1dcbc57bb17d594d76d6f2c7c1fa
alfisnig <valerasafin2005@gmail.com>
2022-03-01 01:26:50 +0300

    added bot config, that auto-parse config.json

--- bot/config.py
+++ bot/config.py
@@ -1,1 +1,16 @@
-API_TOKEN = '1803929720:AAExLiBKNyQqAvYXZuC52e6G_mGxekKWK1E'
+import json
+import logging
+from constants import JSON_FILE_PATH, LOG_FILE_PATH
+
+
+logging.getLogger(LOG_FILE_PATH)
+
+with open(JSON_FILE_PATH, "r", encoding="UTF-8") as json_file:
+    json_config: dict = json.load(json_file)
+
+
+API_TOKEN = json_config.get("API_TOKEN", None)
+if API_TOKEN is None:
+    logging.error(f'API_TOKEN HAS NO VALUE IN JSON FILE f"{JSON_FILE_PATH}"')
+
+REPLY_KEYBOARDS = json_config.get("REPLY_KEYBOARDS", [])
