b6c064752da7313cef4903b6e8228495c31c7286
alfisnig <valerasafin2005@gmail.com>
2022-03-01 01:27:36 +0300

    added new constants and json checking

--- constants.py
+++ constants.py
@@ -1,4 +1,5 @@
 import os
+import json
 
 
 ROOT_DIR = os.path.dirname(os.path.abspath(__file__))
@@ -6,3 +7,13 @@ CATALOG_DB_NAME = "catalog.db"
 CATALOG_DB_PATH = os.path.join(ROOT_DIR, CATALOG_DB_NAME)
 LOG_FILE_NAME = "logs.log"
 LOG_FILE_PATH = os.path.join(ROOT_DIR, LOG_FILE_NAME)
+JSON_FILE_NAME = "config.json"
+JSON_FILE_PATH = os.path.join(ROOT_DIR, JSON_FILE_NAME)
+JSON_DEFAULT_SCTRUCTURE = {"API_TOKEN": "", "REPLY_KEYBOARDS": ["Aspendos", "Elite", "File Air", "Inter", "Martin",
+                                                                "Nil", "Sahra", "Serit Sert File", "Sert Air File",
+                                                                "Sert File", "Toskano"]}
+
+# create config .json file if not exist
+if not os.path.exists(JSON_FILE_PATH):
+    with open(JSON_FILE_PATH, 'w', encoding="UTF-8") as json_file:
+        json.dump(JSON_DEFAULT_SCTRUCTURE, json_file, indent=4, sort_keys=True)
